4276c1ceabe22d0ed77013ff529d228acaa0b168
saoriHANDA <shandax@gmail.com>
2026-04-16 10:37:01 +0900

    Touch and playmusic

---
+++ 03_Basicset_Combination/09_Touch-Speaker/09_Touch-Speaker.ino
@@ -0,0 +1,122 @@
+const int touchPin = 3; //3:ConnectorA 4:ConnectorB
+const int spkrPin = 4; //3:ConnectorA 4:ConnectorB
+
+#define DO 261.6
+#define _DO 277.18
+#define RE 293.665
+#define _RE 311.127
+#define MI 329.63
+#define FA 349.228
+#define _FA 369.994
+#define SO 391.995
+#define _SO 415.305
+#define RA 440
+#define _RA 466.164
+#define TI 493.883
+#define octDO 523.251
+
+
+void playmusic(){
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+  ledcWriteTone(spkrPin, RE);
+  delay(250);
+  ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, FA);
+  delay(250);
+  ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, RE);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(250);
+  ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, FA);
+  delay(250);
+  ledcWriteTone(spkrPin, SO);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(250);
+  ledcWriteTone(spkrPin, SO);
+  delay(250);
+  ledcWriteTone(spkrPin, FA);
+  delay(250);
+  ledcWriteTone(spkrPin, MI);
+  delay(250);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+  ledcWriteTone(spkrPin, 0);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+  ledcWriteTone(spkrPin, 0);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+  ledcWriteTone(spkrPin, 0);
+  delay(250);
+  ledcWriteTone(spkrPin, DO);
+  delay(250);
+  ledcWriteTone(spkrPin, 0);
+  delay(250);
+
+  ledcWriteTone(spkrPin, DO);
+  delay(125);
+  ledcWriteTone(spkrPin, DO);
+  delay(125);
+  ledcWriteTone(spkrPin, RE);
+  delay(125);
+  ledcWriteTone(spkrPin, RE);
+  delay(125);
+  ledcWriteTone(spkrPin, MI);
+  delay(125);
+  ledcWriteTone(spkrPin, MI);
+  delay(125);  
+  ledcWriteTone(spkrPin, FA);
+  delay(125);
+  ledcWriteTone(spkrPin, FA);
+  delay(125);  
+  ledcWriteTone(spkrPin, MI);
+  delay(125);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(125);
+  ledcWriteTone(spkrPin, RE);
+  delay(125);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(125);
+  ledcWriteTone(spkrPin, DO);
+  delay(125);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(125);
+
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(touchPin, INPUT);
+  pinMode(spkrPin, OUTPUT);
+  ledcAttach(spkrPin, 12000, 8); //Pin setting(Pin num, Max frequency, Resolution)
+
+}
+
+void loop() {
+  if (digitalRead(touchPin) == HIGH) {
+   Serial.println("music");
+   playmusic();
+  
+  } else {
+    Serial.println("...");
+
+  }
+  
+delay(3000);
+}
+
+
